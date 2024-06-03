f98e8a0ea67d1987f17b7d937d05b3d382053712
NKhank11 <128170688+NKhank11@users.noreply.github.com>
2024-06-03 15:02:22 +0700

    Delete 2024-22120157-QLDeAn directory

--- 2024-22120157-QLDeAn/FileOnTap.sql
+++
@@ -1,67 +0,0 @@
-﻿use DB_QLDeAn_22120157
-
-/*
-select * from NHANVIEN
-select * from PHONGBAN
-select * from DIADIEM_PHG
-select * from DEAN
-select * from CONGVIEC
-select * from PHANCONG
-select * from THANNHAN
-*/
-
-
-----CÁC CÂU TRUY VẤN ĐƠN GIẢN
-
---1. Tìm những nhân viên làm việc ở phòng số 4
---2. Tìm những nhân viên có mức lương trên 30000
---3, 4. Tìm các nhân viên có mức lương trên 25,000 ở phòng 4 hoặc các nhân viên có mức lương
---trên 30,000 ở phòng 5
---5. Cho biết họ tên đầy đủ của các nhân viên ở TP HCM
---6. Cho biết ngày sinh và địa chỉ của nhân viên Hoang Dinh Hieu
---7. Cho biết danh sách các công việc có tên liên quan đến sản phẩm X.
-
-
-----PHÉP KẾT
---8. Cho biết danh sách các công việc của đề án “Sản phẩm X”.
---9. Cho biết những công việc được thực hiện tại Hà Nội.
---10. Cho biết những công việc mà nhân viên Hoàng Đình Hiếu đảm trách
---11. Cho biết các công việc mà nhân viên phòng Nghiên cứu đảm trách
---12. Với mỗi phòng ban, cho biết tên phòng ban và địa điểm phòng
---13. Tìm tên những người trưởng phòng của từng phòng ban
---14. Tìm TENDA, MADA, DDIEM_DA, PHONG, TENPHG, MAPHG, TRPHG, NG_NHANCHUC
---15. Tìm tên và địa chỉ của tất cả các nhân viên của phòng "Nghien cuu".
---16. Tìm tên những nữ nhân viên và tên người thân của họ
---17. Với mọi đề án ở "Ha Noi", liệt kê các mã số đề án (MADA), mã số phòng ban chủ trì đề án (PHONG), họ tên trưởng phòng
---(HONV, TENLOT, TENNV) cũng như địa chỉ (DCHI) và ngày sinh (NGSINH) của người ấy.
---18. Với mỗi nhân viên, cho biết họ tên nhân viên và họ tên người quản lý trực tiếp của nhân viên đó
---19. Với mỗi nhân viên, cho biết họ tên nhân viên và họ tên trưởng phòng của phòng ban mà nhân viên đó làm việc
---20. Tên những nhân viên phòng số 5 có tham gia vào đề án "San pham X" và nhân viên này do "Nguyen Thanh Tung" quản lý
---trực tiếp. Cho biết họ tên nhân viên (HONV, TENLOT, TENNV) và tên các đề án mà nhân viên ấy tham gia nếu có.
-
-
-----GOM NHÓM
---21. Cho biết phòng ban đảm trách nhiều công việc nhất.
---22. Cho biết đề án ở thành phố HCM mà có nhiều công việc nhất.
---23. Với mỗi đề án, liệt kê tên đề án (TENDA) và tổng số giờ làm việc một tuần của tất cả các nhân viên tham dự đề án đó.
---24. Với mỗi nhân viên, cho biết họ và tên nhân viên và nhân viên đó có bao nhiêu thân nhân
---25. Với mỗi phòng ban, liệt kê tên phòng ban (TENPHG) và lương trung bình của những nhân viên làm việc cho phòng ban đó.
---26. Lương trung bình của tất cả các nữ nhân viên
---27. Với các phòng ban có mức lương trung bình trên 30,000, liệt kê tên phòng ban và số lượng nhân viên của phòng ban đó.----CÂU TRUY VẤN LỒNG--28. Với mỗi đề án, chọn ra công việc mà nhân viên tham gia phải làm lâu nhất.
---29. Cho biết đề án mà nhân viên phòng Nghiên cứu không hề tham gia.
---30. Cho biết danh sách các đề án (MADA) có: nhân công với họ (HONV) là ‘Dinh’ hoặc , có người trưởng phòng chủ trì đề án
---với họ (HONV) là ‘Dinh’.
---31. Danh sách những nhân viên (HONV, TENLOT, TENNV) có trên 2 thân nhân.
---32. Danh sách những nhân viên (HONV, TENLOT, TENNV) không có thân nhân nào.
---33. Danh sách những trưởng phòng (HONV, TENLOT, TENNV) có tối thiểu một thân nhân.
---34. Tìm họ (HONV) của những trưởng phòng chưa có gia đình.
---35. Cho biết họ tên nhân viên (HONV, TENLOT, TENNV) có mức lương trên mức lương trung bình của phòng "Nghien cuu"
---36. Cho biết tên phòng ban và họ tên trưởng phòng của phòng ban có đông nhân viên nhất.
---37. Tìm họ tên (HONV, TENLOT, TENNV) và địa chỉ (DCHI) của những nhân viên làm việc cho một đề án ở ‘TP HCM’
---nhưng phòng ban mà họ trực thuộc lại không tọa lạc ở thành phố ‘TP HCM’ .
---38. Tổng quát câu 16, tìm họ tên và địa chỉ của các nhân viên làm việc cho một đề án ở một thành phố nhưng phòng ban mà họ
---trực thuộc lại không toạ lạc ở thành phố đó.----PHÉP CHIA--39. Cho biết đề án má tất cả công việc của nó đều đã được phân công.
---40. Cho biết đề án được tất cả nhân viên phòng Nghiên cứu tham gia..
---41. Danh sách những nhân viên (HONV, TENLOT, TENNV) làm việc trong mọi đề án của công ty
---42. Danh sách những nhân viên (HONV, TENLOT, TENNV) được phân công tất cả đề án do phòng số 4 chủ trì.
---43. Tìm những nhân viên (HONV, TENLOT, TENNV) được phân công tất cả đề án mà nhân viên Hoang Dinh Hieu làm việc
--- 2024-22120157-QLDeAn/LyThuyet.sql
+++
@@ -1,29 +0,0 @@
-﻿use DB_QLDeAn_22120157
-
---Xem dữ liêu các bảng
-/*
-select * from NHANVIEN
-select * from PHONGBAN
-select * from DIADIEM_PHG
-select * from DEAN
-select * from CONGVIEC
-select * from PHANCONG
-select * from THANNHAN
-*/
-
-
-
---Các câu truy vấn đơn giản
-
---1. Tìm những nhân viên làm việc ở phòng số 4 select *from NhanVienwhere Phg = 4--2. Tìm những nhân viên có mức lương trên 30000select *from NhanVienwhere Luong > 30000--3, 4.Tìm các nhân viên có mức lương trên 25,000 ở phòng 4 hoặc các nhân viên có mức lương trên 30,000 ở phòng 5select *from NhanVienwhere Luong > 25000 and Phg = 4unionselect *from NhanVienwhere Luong > 30000 and Phg = 5--5. Cho biết họ tên đầy đủ của các nhân viên ở TP HCMselect *from NhanVienwhere DChi like '%Tp HCM'--6. Cho biết ngày sinh và địa chỉ của nhân viên Hoang Dinh Hieuselect NgSinh, DChifrom NhanVienwhere HoNV = 'Hoang' and TenLot = 'Dinh' and TenNV = 'Hieu'--7. Cho biết danh sách các công việc có tên liên quan đến sản phẩm X.select *from CongViecwhere TenCongViec like N'%sản phẩm X%'--Phép kết--8. Cho biết danh sách các công việc của đề án “Sản phẩm X”.select CV.*from CongViec CVjoin DeAn DA on DA.MaDA = CV.MaDAwhere DA.TenDA = N'Sản phẩm X'--9. Cho biết những công việc được thực hiện tại Hà Nội.select CV.*from CongViec CVjoin DeAn DA on DA.MaDA = CV.MaDAjoin DiaDiem_Phg DD on DD.MaPhg = DA.Phongwhere DD.DiaDiem = N'Hà Nội'--10. Cho biết những công việc mà nhân viên Hoàng Đình Hiếu đảm tráchselect CV.*from CongViec CVjoin PhanCong PC on PC.MaDA = CV.MaDAjoin NhanVien NV on NV.MaNV = PC.MaNVienwhere HoNV = N'Hoàng' and TenLot = N'Đình' and TenNV = N'Hiếu'--11. Cho biết các công việc mà nhân viên phòng Nghiên cứu đảm tráchselect distinct CV.*from CongViec CVjoin PhanCong PC on PC.MaDA = CV.MaDAjoin NhanVien NV on NV.MaNV = PC.MaNVienjoin PhongBan PB on PB.MaPhg = NV.Phgwhere PB.TenPhg = N'Nghiên cứu'--12. Với mỗi phòng ban, cho biết tên phòng ban và địa điểm phòngselect *from PhongBan PBleft join DiaDiem_Phg DD on DD.MaPhg = PB.MaPhg--13. Tìm tên những người trưởng phòng của từng phòng banselect *from PhongBan PBleft join NhanVien NV on NV.MaNV = PB.TrPhg--Q17: Với mọi đề án ở 'Hà Nội', liệt kê các mã đề án, mã số phòng ban chủ trì để án, họ tên trưởng phòng cũng như địa chỉ và ngày sinh
-
-select DA.MaDA, PB.MaPhg, NV.HoNV, NV.TenLot, NV.TenNV, NV.DChi, NV.NgSinh
-from DeAn DA
-left join PhongBan PB on DA.Phong = PB.MaPhg
-left join NhanVien NV on PB.TrPhg = NV.MaNV
-where DA.DDiem_DA = N'Hà Nội';
-
---Q18: Với mỗi nhân viên, cho biết họ tên nhân viên và họ tên người quản lý trực tiếp của nhân viên đó
-select (NV1.HoNV + ' ' + NV1.TenLot + ' ' + NV1.TenNV) 'Họ tên nhân viên', (NV2.HoNV + ' ' + NV2.TenLot + ' ' + NV2.TenNV) 'Họ tên người quản lý'
-from NhanVien NV1
-left join NhanVien NV2 on NV1.Ma_NQL = NV2.MaNV;
